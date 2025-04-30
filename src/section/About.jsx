033df5f679467e364dc03ee30333d8c720992726
Nishit <nishitku123@gmail.com>
2025-04-30 17:27:26 +0530

    some improvement 2

--- src/section/About.jsx
+++ src/section/About.jsx
@@ -1,8 +1,10 @@
+
 const About = ({ data }) => (
     <section className="p-6 text-white bg-gray-900">
       <h1 className="text-3xl font-bold mb-2">About Me</h1>
       <img src={data.image} alt="Profile" className="w-32 rounded-full mb-4" />
       <p>{data.bio}</p>
+      <a href="https://github.com/nishitk7417" target="_blank">{data.github}</a>
     </section>
   );
   
